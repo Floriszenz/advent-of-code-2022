677f9fca71f8e5af7abb45073dc9b57652b03fc2
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2023-10-10 16:19:24 +0200

    refactor(js-node): improve code for day 3 challenge

--- js-node/day3/src/main.js
+++ js-node/day3/src/main.js
@@ -1,16 +1,14 @@
 import { open } from "node:fs/promises";
 import { argv } from "node:process";
 
-const filePath = argv.at(2);
-
-const rucksackList = await open(filePath);
-let sumOfPriorities = 0;
-
-for await (const rucksack of rucksackList.readLines()) {
+/**
+ * @param {string} rucksack
+ */
+function findCommonItemInRucksack(rucksack) {
   const firstCompartment = rucksack.substring(0, rucksack.length / 2);
   const secondCompartment = rucksack.substring(rucksack.length / 2);
 
-  // Find common item
+  /** @type {string} */
   let commonItem;
 
   firstHalfLoop: for (const firstItem of firstCompartment) {
@@ -22,19 +20,53 @@ for await (const rucksack of rucksackList.readLines()) {
     }
   }
 
-  if (!commonItem) {
-    throw new Error(`Couldn't find any common item in rucksack "${rucksack}"`);
-  }
+  return commonItem;
+}
 
-  const commonCharCode = commonItem.charCodeAt(0);
+/**
+ * @param {string} item
+ */
+function mapItemToPriority(item) {
+  const charCode = item.charCodeAt(0);
+  const a = 97,
+    z = 122,
+    A = 65,
+    Z = 90;
 
-  // Map commonItem to priority
-  if (commonCharCode >= 97 && commonCharCode <= 122) {
-    sumOfPriorities += commonCharCode - 97 + 1;
-  } else if (commonCharCode >= 65 && commonCharCode <= 90) {
-    sumOfPriorities += commonCharCode - 65 + 27;
+  if (charCode >= a && charCode <= z) {
+    return charCode - a + 1;
+  } else if (charCode >= A && charCode <= Z) {
+    return charCode - A + 27;
   }
 }
 
-console.log(`Sum of all priorities: ${sumOfPriorities}`);
-await rucksackList.close();
+/**
+ * @param {string} filePath
+ */
+async function calculateSumOfIndividualPriorities(filePath) {
+  const rucksackList = await open(filePath);
+  let sumOfPriorities = 0;
+
+  for await (const rucksack of rucksackList.readLines()) {
+    const commonItem = findCommonItemInRucksack(rucksack);
+
+    if (!commonItem) {
+      throw new Error(
+        `Couldn't find any common item in rucksack "${rucksack}"`,
+      );
+    }
+
+    sumOfPriorities += mapItemToPriority(commonItem);
+  }
+
+  await rucksackList.close();
+
+  return sumOfPriorities;
+}
+
+const filePath = argv.at(2);
+
+let sumOfIndividualPriorities =
+  await calculateSumOfIndividualPriorities(filePath);
+
+console.log(`Sum of individual priorities: ${sumOfIndividualPriorities}`);
