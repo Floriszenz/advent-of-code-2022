d711595386feab8ad2f2f76b03f6154dd9231e7a
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2023-10-15 20:30:10 +0200

    feat(js-node): add solution for day 7 challenge (second part)

--- js-node/day7/src/main.js
+++ js-node/day7/src/main.js
@@ -19,3 +19,26 @@ const totalSizeOfSmallDirectories = filesystem.traverseFilesystem(
 console.log(
   `Total size of directories that are each at most 100'000 bytes big: ${totalSizeOfSmallDirectories}`,
 );
+
+const AVAILABLE_DISK_SPACE = 70_000_000;
+const REQUIRED_DISK_SPACE = 30_000_000;
+const unusedDiskSpace = AVAILABLE_DISK_SPACE - filesystem.root.size;
+const diskSpaceThatMustBeFreed = REQUIRED_DISK_SPACE - unusedDiskSpace;
+
+const totalSizeOfDirectoryToDelete = filesystem.traverseFilesystem(
+  (minimum, currentDirectory) => {
+    if (
+      currentDirectory.size >= diskSpaceThatMustBeFreed &&
+      currentDirectory.size < minimum
+    ) {
+      return currentDirectory.size;
+    }
+
+    return minimum;
+  },
+  Infinity,
+);
+
+console.log(
+  `Total size of directory that should be deleted: ${totalSizeOfDirectoryToDelete}`,
+);
